d1a8b0028afce39ab0b99b3d2285427e37f8a9e4
kajudaju <kajudaju@hotmail.com>
2024-02-16 14:13:16 -0500

    mi scrip de hub

---
+++ networkscrip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#network testing for git bash on windows
+
+echo "Network Testing Script"
+
+#Check Internet connectivity
+
+echo "1. chechinh Internet Connectivity"
+
+ping -c 4 www.google.com
+
+echo "----------------------" 
+

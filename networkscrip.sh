b1eba1c0de1cf34a79b8a7004461b555e156a391
kajudaju <kajudaju@hotmail.com>
2024-02-16 16:37:48 -0500

    agregando el archivo  test-security final code (hey compa soy tu buen compa jajaj)

--- networkscrip.sh
+++ networkscrip.sh
@@ -24,6 +24,28 @@ echo "3. Displaying Routing Table"
 netstat -rm 
 echo "--------------------------"
 
+#check open ports on local machine
+read -p "enter the IP Address or hostname to check open ports on: "remote_host 
+echo "checking Open PORTS ON $remote_host"
+nmap -p- $remote_host 
+echo "-------------------------"
+
+# traceroute to a remote host
+read -p "enter the Ip address or hostname to trace route to: "trace_host
+echo "trace route to : $trace_host"
+tracert $trace_host
+echo "-------------------------------"
+
+#Test banwidth using speed test cli for windows 
+read -p "Do you want to test nbandwith with speedtest CLI? (y/n): "speedtest_option
+
+fi ["$speedtest_option"== "y"]; then 
+./speedtestbyookla_64
+
+fi
+echo "-------------------------------"
+
+echo "Network testing completed"
 
 
 

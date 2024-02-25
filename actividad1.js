84945d99841b109e0d9d06158f7559de245c31cf
bulcano720 <kajudaju@hotmail.com>
2024-02-25 18:30:05 -0500

    punto6 de la activad 1

--- actividad1.js
+++ actividad1.js
@@ -12,3 +12,16 @@ objeto_a.bar = 25;
 
 console.log("Valor de foo:", objeto_a.foo);
 console.log("Valor de bar:", objeto_a.bar);
+
+class B {
+    constructor(foo, bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+}
+
+// Ejemplo de uso:
+let objeto_b = new B("Hola", 42);
+
+console.log("Valor de foo:", objeto_b.foo);
+console.log("Valor de bar:", objeto_b.bar);

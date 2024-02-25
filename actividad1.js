8d5a8bfa879cce0e5dc62153ea8432911c363a06
bulcano720 <kajudaju@hotmail.com>
2024-02-25 17:50:18 -0500

    punto3 de la actividad

---
+++ actividad1.js
@@ -0,0 +1,14 @@
+class A {
+    constructor() {
+        this.foo = null;
+        this.bar = null;
+    }
+}
+
+// Ejemplo de uso:
+let objeto_a = new A();
+objeto_a.foo = "Hola gente ";
+objeto_a.bar = 25;
+
+console.log("Valor de foo:", objeto_a.foo);
+console.log("Valor de bar:", objeto_a.bar);
